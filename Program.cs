6a26f02501485c0090ef4313179bb8bcebcec5bf
Eryck Santos <dev.erycksantos@gmail.com>
2025-01-28 10:49:39 -0300

    First draft of the project. Adding functions for operations.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+Divisao();
+
+static void Soma()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    double v1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    double result = v1 + v2;
+    Console.WriteLine($"{v1} + {v2} = {result}");
+
+}
+
+static void Subtracao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    double v1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    double result = v1 - v2;
+    Console.WriteLine($"{v1} - {v2} = {result}");
+}
+
+static void Multiplicacao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    double v1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    double result = v1 * v2;
+    Console.WriteLine($"{v1} x {v2} = {result}");
+}
+
+static void Divisao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    double v1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    double result = v1 / v2;
+    Console.WriteLine($"{v1} ÷ {v2} = {result}");
+}
